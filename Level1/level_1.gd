09408f75cca39fc3ef74b1c125b11b7ea3e87aac
ImSatanJunior <cholder1234567890@gmail.com>
2023-07-08 00:07:48 +0100

    Turn Walls Into Floor

---
+++ Level1/level_1.gd
@@ -0,0 +1,19 @@
+extends Node2D
+
+var tilemap
+
+var tilesRemoved = 0;
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	tilemap = get_node_or_null("InteractiveTilemap")
+
+		
+func _unhandled_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+			var clickedTile = tilemap.local_to_map(event.position)
+			print(tilemap.get_cell_tile_data(0, clickedTile).get)
+			var tileId = tilemap.get_cell_tile_data(0, clickedTile).get_custom_data("Name")
+			if tileId == "Wall":
+				tilemap.set_cell(0, clickedTile, 0,Vector2i(0, -0))
